a1a1c9dfd15738a800b32c356cdeced16f23cdd4
RobertBorbely <respectsas@gmail.com>
2016-03-22 17:26:15 +0000

    Added new menu & mobile menu

--- js/functions.js
+++ js/functions.js
@@ -20,7 +20,22 @@
     $intro.css("margin-bottom", 0);
   }
   
+  function MobileMenuInit() {
+    var menuToggle = $('#js-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass("show");
+  
+    menuToggle.on('click', function(e) {
+      e.preventDefault();
+      $('#js-navigation-menu').slideToggle(function(){
+        if($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
+        }
+      });
+    });  
+  }
+  
   $(function(){
     addNegativeMarginToIntro();
+    MobileMenuInit();
   });
 })(jQuery);
